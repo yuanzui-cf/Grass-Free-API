e81b6caad559cff30faac5a6710fef5d265b24a3
yuanzui-cf <grassblogstu@gmail.com>
2022-08-23 23:59:01 +0800

    feat: Renew the frame

--- functions.js
+++ functions.js
@@ -5,5 +5,22 @@ module.exports = {
                 return Math.floor(Math.random() * max);
             }
         }
+    },
+    log: {
+        log: content => {
+            console.log(`[LOG] ${content}`);
+        },
+        warn: content => {
+            console.log(`\x1B[33m[WARN]\x1B[0m ${content}`);
+        },
+        err: content => {
+            console.log(`\x1B[31m[ERROR]\x1B[0m ${content}`);
+        },
+        error: content => {
+            console.log(`\x1B[31m[ERROR]\x1B[0m ${content}`);
+        },
+        success: content => {
+            console.log(`\x1B[32m[SUCCESS]\x1B[0m ${content}`);
+        }
     }
 };
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
-Grass Free API [ver 2.0.0-alpha]
+Grass Free API [ver 2.0.0-alpha2]
 Author: Grass Development Team
 Website: https://mystu.net/
 Github: https://github.com/yuanzui-cf/Grass-Free-API/
@@ -13,13 +13,17 @@ const os = require("os"),
       https = require("https"),
       func = require("./functions"),
       server = express();
+console.log("Grass Free API [v2.0.0-alpha2]");
+console.log("Copyright (c) 2022 Grass Development Team.");
 let port = 9000;
 let include = [];
 let config;
 if(fs.existsSync("./config.json")){
     config = fs.readJsonSync("./config.json", "UTF-8");
+    func.log.success("Successfully read the config file.");
 }
 if(config){
+    func.log.success("Successfully parse the config.");
     if(
         config.port &&
         typeof config.port == "number" &&
@@ -27,16 +31,25 @@ if(config){
         config.port <= 25565
     ){
         port = config.port;
+        func.log.success(`Successfully change the port to the \x1B[32m${port}\x1B[0m`);
+    }
+    else{
+        func.log.warn(`You didn't set the port. The app will start on the default port \x1B[32m9000\x1B[0m`);
     }
     if(
         config.include &&
         typeof config.include == "object"
     ){
         include = config.include;
+        func.log.success(`Success load the include lists.`);
+    }
+    else{
+        func.log.warn(`You didn't set the include. That is meant, you cannot do anything by the API.`);
     }
 }
 if(!fs.existsSync("./app")){
     fs.mkdirSync("./app");
+    func.log.success(`Successfully create the app dir.`);
 }
 server.use(cors());
 let app = [];
@@ -45,10 +58,32 @@ module.exports = {
     fs: fs,
     os: os,
     func: func.func,
+    log: func.log,
     maindir: __dirname,
     http: http,
     https: https
 };
+for(let i = 0; i < include.length; i++){
+    const app_tmp = {
+        name: include[i].name,
+        path: include[i].path.replace(/(.app)|(.mod)/gi, "/main")
+                             .replace(/%include%/gi, "./include"),
+        enable: false
+    };
+    if(fs.existsSync(app_tmp.path + ".js")){
+        require(app_tmp.path);
+        func.log.success(`Successfully load the plugin '${app_tmp.name}'.`);
+        app_tmp.enable = true;
+    }
+    else{
+        func.log.warn(`Failed to load plugin '${app_tmp.name}', please check the plugin.`);
+    }
+    app_tmp.path = undefined;
+    app[i] = app_tmp;
+}
+if(fs.existsSync("./public")){
+    server.use("/", express.static("./public"));
+}
 server.get("/status", (req, res) => {
     const data = {
         system: {
@@ -60,30 +95,27 @@ server.get("/status", (req, res) => {
     };
     res.send( data );
 });
-for(let i = 0; i < include.length; i++){
-    const app_tmp = {
-        name: include[i].name,
-        path: include[i].path.replace(/(.app)|(.mod)/gi, "/main")
-                             .replace(/%include%/gi, "./include"),
-        enable: false
-    };
-    if(fs.existsSync(app_tmp.path + ".js")){
-        require(app_tmp.path);
-        app_tmp.enable = true;
-    }
-    app_tmp.path = undefined;
-    app[i] = app_tmp;
-}
-if(fs.existsSync("./public")){
-    server.use("/", express.static("./public"));
-}
-server.get("*", (req, res) => {
-    res.send({
-        name: "Grass Free API",
-        version: "v2.0.0-alpha",
-        plugin: app
+if(
+    config &&
+    config.allValueShowed
+){
+    server.get("*", (req, res) => {
+        res.send({
+            name: "Grass Free API",
+            version: "v2.0.0-alpha",
+            plugin: app
+        });
     });
-});
+}
+else{
+    server.get("/about", (req, res) => {
+        res.send({
+            name: "Grass Free API",
+            version: "v2.0.0-alpha",
+            plugin: app
+        });
+    });
+}
 server.listen(port, () => {
-    console.log(`Grass Free API was listened on ${port}`);
+    func.log.success(`Grass Free API was listened on :${port}`);
 });
